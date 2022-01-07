fe2a5e5ebafd26e89c0030ff0bdf02e417b3344d
Michael Zhao <michael.yj.zhao@gmail.com>
2022-01-07 14:11:04 +1100

    Deploy and app logo

--- ExpenseTracker/Models/Category.swift
+++ ExpenseTracker/Models/Category.swift
@@ -11,23 +11,23 @@ import SwiftUI
 enum Category: String, CaseIterable {
     
     case essentials
-    case giftstoothers
+    case gifts
     case subscriptions
-    case eatingout
+    case food
     case dates
-    case giftstoself
-    case ravesupplement
+    case consumerism
+    case ravesup
     case other
     
     var systemNameIcon: String {
         switch self {
         case .essentials: return "cart.fill"
-        case .giftstoothers: return "gift.fill"
+        case .gifts: return "gift.fill"
         case .subscriptions: return "tv.music.note"
-        case .eatingout: return "fork.knife"
+        case .food: return "fork.knife"
         case .dates: return "heart.circle"
-        case .giftstoself: return "face.smiling"
-        case .ravesupplement: return "pills"
+        case .consumerism: return "face.smiling"
+        case .ravesup: return "pills"
         case .other: return "archivebox"
         }
     }
@@ -36,11 +36,11 @@ enum Category: String, CaseIterable {
         switch self {
         case .essentials: return Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1))
         case .subscriptions: return Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1))
-        case .giftstoothers: return Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
-        case .eatingout: return Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
+        case .gifts: return Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
+        case .food: return Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
         case .dates: return Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
-        case .giftstoself: return Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
-        case .ravesupplement: return Color(#colorLiteral(red: 0.1, green: 0.1, blue: 0.1, alpha: 0.3))
+        case .consumerism: return Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
+        case .ravesup: return Color(#colorLiteral(red: 0.1, green: 0.1, blue: 0.1, alpha: 0.3))
         case .other: return Color(#colorLiteral(red: 0.1, green: 0.1, blue: 0.1, alpha: 0.3))
         }
     }
